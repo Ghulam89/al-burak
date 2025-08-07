0d793c411269b0e9d0eef0f7aa72f252bf893a93
Ghulam89 <gm6681328@gmail.com>
2025-08-07 00:53:02 -0700

    update

--- src/components/login/ProtectedRoute.jsx
+++ src/components/login/ProtectedRoute.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+  const isLoggedIn = localStorage.getItem("userId") !== null;
+
+ 
+
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
